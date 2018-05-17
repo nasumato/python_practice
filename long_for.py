12ad8e1d14efbb3a593c6c377c75c99902669b40
草ヶ谷誠 <kusagayamakoto@kusatanimakotonoMacBook-Air.local>
2018-05-17 10:06:51 +0900

    add new file

---
+++ long_for.py
@@ -0,0 +1,12 @@
+
+
+long_word=""
+for word in ['hkjn', 'data','dhjkf']:
+    long_word += "{}ghjlghjk".format(word)
+
+long_word=[]
+for word in ['hkjn', 'data','dhjkf']:
+    long_word.append("{}ghjlghjk".format(word))
+
+new_long_word =''.join(long_word)
+print(new_long_word)
